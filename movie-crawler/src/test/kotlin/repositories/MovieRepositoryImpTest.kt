55a5e2fcd6b895ddd4ffc2d2c69f2609d3155fb4
saied <saiedmomen@gmail.com>
2018-11-04 08:36:15 +0330

    add unit test for getInfoLessMovies

--- movie-crawler/src/test/kotlin/repositories/MovieRepositoryImpTest.kt
+++ movie-crawler/src/test/kotlin/repositories/MovieRepositoryImpTest.kt
@@ -1,11 +1,15 @@
 package repositories
 
 import com.mongodb.client.MongoCollection
+import com.squareup.moshi.Moshi
+import di.netwokModule
 import models.Movie
+import models.OmdbData
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
 import org.koin.standalone.StandAloneContext.startKoin
 import org.koin.standalone.StandAloneContext.stopKoin
+import org.koin.standalone.get
 import org.koin.standalone.inject
 import org.koin.test.KoinTest
 import org.litote.kmongo.deleteMany
@@ -20,7 +24,7 @@ internal class MovieRepositoryImpTest: KoinTest{
 
     @BeforeAll
     fun setup(){
-        startKoin(listOf(testModule))
+        startKoin(listOf(testModule, netwokModule))
     }
 
     @BeforeEach
@@ -38,8 +42,22 @@ internal class MovieRepositoryImpTest: KoinTest{
     }
 //
     @Test
-    fun `save 100 movies to db and retrieve them`(){
+    fun `getInfoLessMovies returns only infoless movies`(){
+        val movie1 = Movie(title = "title1", releaseDate = 0L)
+        val movie2 = Movie(title = "title2", releaseDate = 0L, omdbData = getSampleOMDBInfo())
+        movieRepository.saveMovies(listOf(movie1, movie2))
 
+        val res = movieRepository.getInfoLessMovies()
+        assert(res.size == 1)
+        assert(res[0].equals(movie1))
+    }
+
+    private fun getSampleOMDBInfo(): OmdbData{
+        val moshi: Moshi = get()
+        val adapter = moshi.adapter(OmdbData::class.java)
+
+        val jsonText = javaClass.getResource("../api/OmdpSample.json").readText()
+        return adapter.fromJson(jsonText)!!
     }
 
     @AfterAll
